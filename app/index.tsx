8584b641485268aa2d589c284adb7db330f461e1
Senyo Dedoo <mrdee@Senyos-MacBook-Air.local>
2024-05-19 21:36:11 +0000

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+
+const App = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href="/home" style={{ color: "blue" }}>
+        Go to Home
+      </Link>
+    </View>
+  );
+};
+
+export default App;
